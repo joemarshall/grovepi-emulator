5dc67da24a1a0440864c659169e07c4693e990a6
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-03-02 14:54:37 +0000

    make executable for ttk version

--- makeexecutable.bat
+++ makeexecutable.bat
@@ -1,4 +1,4 @@
-pyinstaller --add-data *.png;. --add-data main.ico;. grovepiemu.py --add-data testfiles/*;testfiles --add-data gpe_utils/pikeys;pikeys -p fakegrovepi 
+pyinstaller --add-data *.png;. --add-data main.ico;. grovepiemu.py --add-data testfiles/*;testfiles --add-data gpe_utils/pikeys;pikeys -p fakegrovepi --add-data Azure-ttk-theme;Azure-ttk-theme
 cd dist\grovepiemu
 grovepiemu.exe
 cd ..\..
--- makeexecutable.mac.sh
+++ makeexecutable.mac.sh
@@ -2,6 +2,6 @@
 
 #pyinstaller --add-data main.ico:. grovepiemu.py --add-data testfiles/*:testfiles --add-data gpe_utils/pikeys:pikeys -p fakegrovepi
 
-pyinstaller --onefile --console --add-data *.png:. --add-data main.ico:. grovepiemu.py --add-data testfiles/*:testfiles --add-data gpe_utils/pikeys:pikeys -p fakegrovepi
+pyinstaller --onefile --console --add-data *.png:. --add-data main.ico:. grovepiemu.py --add-data testfiles/*:testfiles --add-data gpe_utils/pikeys:pikeys --add-data Azure-ttk-theme:Azure-ttk-theme -p fakegrovepi
 
 
