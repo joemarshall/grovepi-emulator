e7b5191a6ce7e6ec184116b32ae9297c986935ef
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-05-06 11:22:35 +0100

    force install of filters and sensors modules

--- makeexecutable.bat
+++ makeexecutable.bat
@@ -1,4 +1,4 @@
-pyinstaller --add-data *.png;. --add-data main.ico;. grovepiemu.py --add-data testfiles/*;testfiles --add-data gpe_utils/pikeys;pikeys -p fakegrovepi --add-data Azure-ttk-theme;Azure-ttk-theme
+pyinstaller --add-data *.png;. --add-data main.ico;. grovepiemu.py --add-data testfiles/*;testfiles --add-data gpe_utils/pikeys;pikeys -p fakegrovepi --add-data Azure-ttk-theme;Azure-ttk-theme --hidden-import sensors --hidden-import graphs --hidden-import filters
 cd dist
 del grovepiemu-windows.zip
 powershell.exe Compress-Archive grovepiemu grovepiemu-windows.zip
